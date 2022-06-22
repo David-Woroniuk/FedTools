6c22d29e5d3919b83452390c9e87decc6046b5c9
David <70294884+David-Woroniuk@users.noreply.github.com>
2022-06-22 22:38:36 +0100

    Update Beigebook.py

--- FedTools/Beigebook.py
+++ FedTools/Beigebook.py
@@ -2,199 +2,242 @@
 
 from __future__ import print_function
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
+from fake_useragent import UserAgent
+from datetime import datetime
 import re
 import pandas as pd
 import pickle
 import threading
 import sys
+import os
 
 
 class BeigeBooks(object):
-  '''
-  This class firstly extracts the release month of each previous Beige Book.
-  Subsequently, the class extracts the Beige Book, placing results into a 
-  DataFrame, indexed by release date.
-  -------------------------------Arguments------------------------------------------- 
-  main_url: Federal Reserve Open Monetary Policy (FOMC) website URL. (str)
-  beige_book_url: Beige Book directory page. (str)
-  start_year: The year which the user wishes to begin parsing from (>=1996). (int)
-  historical_split: year considered as historical (historical vs current archive list). (int) 
-  verbose: boolean determining printing during scraping. (bool)
-  thread_num: the number of threads to use for web scraping  
-  -------------------------------Returns--------------------------------------------- 
-  dataset - a DataFrame containing Beige Books, indexed by release date.
-  '''
+    """
+    This class firstly checks that the input arguments are of the correct type, followed
+    by extracting the release month of each previous Beige Book. Subsequently, the Beige Book
+    data is extracted, placing the results into a Pandas DataFrame.
 
-  def __init__(self,
-               main_url='https://www.federalreserve.gov',
-               beige_book_url='https://www.federalreserve.gov/monetarypolicy/beige-book-default.htm',
-               start_year = None,
-               historical_split=2019,
-               verbose=True,
-               thread_num=10
-               ):
-      
-    if start_year is None:
-      start_year = 1996
-    elif start_year < 1996:
-      raise TypeError("The start_year argument must be 1996 or later.")
-    elif isinstance(start_year, int) is False:
-      raise TypeError("The start_year argument must be a integer object.")
+    :param main_url: the Federal Reserve Open Monetary Policy (FOMC) website URL. (str)
+    :param beige_book_url: the Beige Book directory page. (str)
+    :param start_year: the year which the user wishes to begin parsing from. (int)
+    :param historical_split: the year considered as historical (historical vs current archive list). (int)
+    :param verbose: boolean determining printing during scraping. (bool)
+    :param thread_num: the number of threads to use for web scraping. (int)
+    :return: dataset: a DataFrame containing meeting minutes, indexed by meeting date. (pd.DataFrame)
 
-    if isinstance(historical_split, int) is False:
-      raise TypeError("The historical_split argument must be an integer object. Please refer to https://www.federalreserve.gov/monetarypolicy/beige-book-archive.htm for the start year")
+    """
 
-    if isinstance(thread_num, int) is False:
-      raise TypeError("The thread_num argument must be an integer object.")
+    def __init__(self,
+                 main_url: str = 'https://www.federalreserve.gov',
+                 beige_book_url: str = 'https://www.federalreserve.gov/monetarypolicy/beige-book-default.htm',
+                 start_year: int = 1996,
+                 historical_split: int = 2019,
+                 verbose: bool = True,
+                 thread_num: int = 10
+                 ):
 
-    self.main_url = main_url
-    self.beige_book_url = beige_book_url
-    self.start_year = start_year
-    self.HISTORICAL_SPLIT = historical_split
-    self.verbose = verbose
-    self.THREAD_NUM = thread_num
-    self.dataset = None
-    self.links = None
-    self.dates = None
-    self.articles = None
+        if not all(isinstance(v, str) for v in [main_url, beige_book_url]):
+            raise TypeError("The 'main_url' and 'beige_book_url' arguments must be string types.")
+        if not all(isinstance(v, int) for v in [start_year, historical_split, thread_num]):
+            raise TypeError("The 'start_year', 'historical_split' and 'thread_num' arguments must be integer types.")
+        if not isinstance(verbose, bool):
+            raise TypeError("The 'verbose' argument must be a boolean type.")
 
-  def _obtain_links(self, start_year):
-    '''
-    internal function which constructs the links of all FOMC meetings, 
-    beggining at 'start_year' and ending at current year.
-    '''
+        self.main_url = main_url
+        self.beige_book_url = beige_book_url
+        self.start_year = start_year
+        self.HISTORICAL_SPLIT = historical_split
+        self.verbose = verbose
+        self.THREAD_NUM = thread_num
+        self.dataset = None
+        self.links = None
+        self.dates = None
+        self.articles = None
 
-    if self.verbose:
-      print("Constructing links.")
+    def _obtain_links(self, start_year: int):
+        """
+        The helper function constructs the links of all FOMC meetings,
+        beginning at the start_year' argument, and continuing to the current
+        date.
 
-    self.links = []
-    Beige_Book_Socket = urlopen(self.beige_book_url)
-    soup = BeautifulSoup(Beige_Book_Socket, 'html.parser')
+        :param start_year: the year at which the link construction begins. (int)
+        """
+        if not isinstance(start_year, int):
+            raise TypeError("The 'start_year' variable must be an integer type.")
 
-    Beige_Books = soup.find_all('a', href=re.compile('^/monetarypolicy/beigebook\d{6}.htm'))
-    self.links = [Beige_Book.attrs['href'] for Beige_Book in Beige_Books]
+        if self.verbose:
+            print("Constructing links between {} and {}".format(start_year, datetime.today().year))
 
-    if start_year <= self.HISTORICAL_SPLIT:
-      for year in range(start_year, self.HISTORICAL_SPLIT + 1):
-        beige_book_annual_url = self.main_url + '/monetarypolicy/beigebook' + str(year) + '.htm'
-        beige_book_annual_socket = urlopen(beige_book_annual_url)
-        bb_annual_soup = BeautifulSoup(beige_book_annual_socket, 'html.parser')
-        historical_statements = bb_annual_soup.findAll('a', text='HTML')
-        for historical_statement in historical_statements:
-          self.links.append(historical_statement.attrs['href'])
+        self.links = []
+        beige_book_socket = self._urlopen_with_ua(self.beige_book_url)
+        soup = BeautifulSoup(beige_book_socket, 'html.parser')
+        beige_books = soup.find_all('a', href=re.compile('^/monetarypolicy/beigebook\\d{6}.htm'))
+        self.links = [beige_book.attrs['href'] for beige_book in beige_books]
 
-  def _find_date_from_link(self, link):
-    '''
-    internal function which finds the meeting date within the relevant link.
-    '''
-    date = re.findall('[0-9]{6}', link)[0]
-    if date[4] == '0':
-      date = "{}/{}/{}".format(date[:4], date[5:6], '01')
-    else:
-      date = "{}/{}/{}".format(date[:4], date[4:6], '01')
+        if start_year <= self.HISTORICAL_SPLIT:
+            for year in range(start_year, self.HISTORICAL_SPLIT + 1):
+                beige_book_annual_url = self.main_url + '/monetarypolicy/beigebook' + str(year) + '.htm'
+                beige_book_annual_socket = self._urlopen_with_ua(beige_book_annual_url)
+                bb_annual_soup = BeautifulSoup(beige_book_annual_socket, 'html.parser')
+                historical_statements = bb_annual_soup.findAll('a', text='Statement')
+                for historical_statement in historical_statements:
+                    self.links.append(historical_statement.attrs['href'])
 
-    return date
+    @staticmethod
+    def _urlopen_with_ua(url: str) -> str:
+        """
+        This helper function adds user agent credentials to the
+        request, enabling the script to interact with the Federal
+        Reserve website.
 
-  def _add_article(self, link, index=None):
-    '''
-    internal function which adds the related article for 1 link to the instance variable.
-    'index' is the index in the article to add to. Due to multithreading, articles must be
-    stored in the correct order.
-    '''
-    # write a dot as progress report:
-    if self.verbose:
-      sys.stdout.write(".")
-      sys.stdout.flush()
+        :param url: the url to be queried, without a user agent. (str)
+        :return: urlopen(req): the url opened using a user agent. (str)
+        """
+        if not isinstance(url, str):
+            raise TypeError("The 'url' argument must be a string type.")
 
-    # append date of article content appropriately:
-    self.dates.append(self._find_date_from_link(link))
+        ua = UserAgent()
+        req = Request(url)
+        req.add_header("user-agent", ua.chrome)
+        return urlopen(req)
 
-    # conditional sockets depending on length of links:
-    if len(link) <= 35:
-      Beige_Book_Output_socket = urlopen(self.main_url + link)
-      Beige_Book_Output = BeautifulSoup(Beige_Book_Output_socket, 'html.parser')
-      paragraph_delimiter = Beige_Book_Output.findAll('p')
-      self.articles[index] = "\n\n".join([paragraph.get_text().strip() for paragraph in paragraph_delimiter])
-    else:
-      Beige_Book_Output_socket = urlopen(link)
-      Beige_Book_Output = BeautifulSoup(Beige_Book_Output_socket, 'html.parser')
-      paragraph_delimiter = Beige_Book_Output.findAll('p')
-      self.articles[index] = "\n\n".join([paragraph.get_text().strip() for paragraph in paragraph_delimiter])
+    @staticmethod
+    def _find_date_from_link(link: str) -> str:
+        """
+        This helper function determines the FOMC meeting date from the relevant link.
+        The function firstly checks that the link is a string type, followed by parsing
+        the string to generate the date. The date string is subsequently returned.
 
-  def _multithreaded_article_retrieval(self):
-    '''
-    internal function which returns all articles. The function utilises multithreading to
-    reduce scraping time.
-    '''
+        :param link: the link string to be parsed for dates. (str)
+        :return: date: the date string parsed from the link string. (str)
+        """
+        if not isinstance(link, str):
+            raise TypeError("The 'link' argument must be a string type.")
 
-    if self.verbose:
-      print("Retrieving articles.")
+        date = re.findall('[0-9]{6}', link)[0]
+        if date[4] == '0':
+            date = "{}/{}/{}".format(date[:4], date[5:6], "01")
+        else:
+            date = "{}/{}/{}".format(date[:4], date[4:6], "01")
+        return date
 
-    self.dates, self.articles = [], [''] * len(self.links)
-    jobs = []
-    # initiate process, start threads:
-    index = 0
-    while index < len(self.links):
-      if len(jobs) < self.THREAD_NUM:
-        thread = threading.Thread(target=self._add_article, args=(self.links[index], index,))
-        jobs.append(thread)
-        thread.start()
-        index += 1
-      else:
-        thread = jobs.pop(0)
-        thread.join()
+    def _add_article(self, link: str, index: int = None):
+        """
+        This helper function adds the related minutes for 1 link to the instance variable.
+        Multithreading stipulates that the articles must be stored in the correct order, where
+        the 'index' argument is the index in the article to add to.
 
-    for thread in jobs:
-      thread.join()
+        :param link: the link to be opened and data generated for. (str)
+        :param index: the index associated with the link. (int)
+        """
+        if not isinstance(link, str):
+            raise TypeError("The 'link' argument must be a string type.")
+        if not isinstance(index, (type(None), int)):
+            raise TypeError("The 'index' argument must either be a None type or a integer type.")
 
-    for row in range(len(self.articles)):
-      self.articles[row] = self.articles[row].strip()
+        if self.verbose:
+            sys.stdout.write(".")
+            sys.stdout.flush()
 
-  def find_beige_books(self):
-    '''
-    This function returns beige book information within a pandas dataframe, 
-    where the index corresponds to the release date.
-    -------------------------------Arguments------------------------------------------ 
-    N/A
-    -------------------------------Returns-------------------------------------------- 
-    dataset - a DataFrame containing Beige Books, indexed by release date.
-    '''
+        self.dates.append(self._find_date_from_link(link))
+        if len(link) <= 35:
+            beige_book_output_socket = self._urlopen_with_ua(self.main_url + link)
+            beige_book_output = BeautifulSoup(beige_book_output_socket, 'html.parser')
+            paragraph_delimiter = beige_book_output.findAll('p')
+            self.articles[index] = "\n\n".join([paragraph.get_text().strip() for paragraph in paragraph_delimiter])
+        else:
+            beige_book_output_socket = self._urlopen_with_ua(link)
+            beige_book_output = BeautifulSoup(beige_book_output_socket, 'html.parser')
+            paragraph_delimiter = beige_book_output.findAll('p')
+            self.articles[index] = "\n\n".join([paragraph.get_text().strip() for paragraph in paragraph_delimiter])
 
-    self._obtain_links(self.start_year)
-    print("Extracting the past {} Beige Books.".format(len(self.links)))
-    self._multithreaded_article_retrieval()
+    def _multithreaded_article_retrieval(self):
+        """
+        This helper function returns all articles associated with each link. The function firstly
+        initiates the threads, as specified by the 'thread_num' argument passed to the class. The
+        function uses each thread to efficiently extract the articles and store the outcome.
+        """
+        if self.verbose:
+            print("Retrieving articles.")
 
-    self.dataset = pd.DataFrame(self.articles, index=pd.to_datetime(self.dates)).sort_index()
-    self.dataset.columns = ['Beige_Book']
+        self.dates, self.articles = [], [''] * len(self.links)
+        jobs = []
+        index = 0
+        while index < len(self.links):
+            if len(jobs) < self.THREAD_NUM:
+                thread = threading.Thread(target=self._add_article, args=(self.links[index], index,))
+                jobs.append(thread)
+                thread.start()
+                index += 1
+            else:
+                thread = jobs.pop(0)
+                thread.join()
 
-    if self.verbose:
-      sys.stdout.write(".")
-      sys.stdout.flush()
+        for thread in jobs:
+            thread.join()
 
-    for i in range(len(self.dataset)):
-      self.dataset.iloc[i, 0] = self.dataset.iloc[i, 0].replace('\n', ' ')
-      self.dataset.iloc[i, 0] = self.dataset.iloc[i, 0].replace('\r', ' ')
-      self.dataset.iloc[i, 0] = self.dataset.iloc[i, 0].replace('\t', '')
-      self.dataset.iloc[i, 0] = self.dataset.iloc[i, 0].replace('\xa0', '')
+        for row in range(len(self.articles)):
+            self.articles[row] = self.articles[row].strip()
 
-    return self.dataset
+    def find_beige_books(self):
+        """
+        This function acts as the main public function of the class, returning the Beige Books
+        by efficiently extracting the information from the FOMC Website. The function then places each
+        Beige Book into a Pandas DataFrame, indexed by the meeting date string.
 
-  def pickle_data(self, directory="../data/Beige_Books.pkl"):
-    '''
-    This function pickles and stores the scraped dataset in a predefined directory.
-    -------------------------------Arguments------------------------------------------ 
-    directory: The filepath where the pickled dataset should be stored.
-    -------------------------------Returns-------------------------------------------- 
-    A pickled dataframe stored within the predefined directory.
-    '''
-    if directory:
-      if self.verbose:
-        print("Pickling Data, writing to {}".format(directory))
-        with open(directory, "wb") as output:
-          pickle.dump(self.dataset, output)
+        :return: dataset: a Pandas DataFrame containing the meeting minutes, indexed by meeting date. (pd.DataFrame)
+        """
+        self._obtain_links(self.start_year)
+        if self.verbose:
+            print("Extracting the past {} Beige Books.".format(len(self.links)))
+        self._multithreaded_article_retrieval()
+
+        self.dataset = pd.DataFrame(self.articles, index=pd.to_datetime(self.dates)).sort_index()
+        self.dataset.columns = ['Beige_Book']
+
+        if self.verbose:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+
+        for i in range(len(self.dataset)):
+            self.dataset.iloc[i, 0] = self.dataset.iloc[i, 0].replace('\n', ' ')
+            self.dataset.iloc[i, 0] = self.dataset.iloc[i, 0].replace('\r', ' ')
+            self.dataset.iloc[i, 0] = self.dataset.iloc[i, 0].replace('\t', '')
+            self.dataset.iloc[i, 0] = self.dataset.iloc[i, 0].replace('\xa0', '')
+
+        return self.dataset
+
+    def pickle_data(self, directory: str) -> bool:
+        """
+        This public function acts as a main public function, for extraction and pickling of
+        the extracted data to the 'directory' argument passed. The function checks that the
+        directory argument is a string type, followed by checking if the directory ends with
+        the appropriate extension. The folder is then created if necessary, followed by the
+        data being written to the pickle file, where a boolean is returned to denote success / failure
+        of the file write operation.
+
+        :param directory: the directory to which the file should be written. (str)
+        :return: bool: determines if the file was written correctly. (bool)
+        """
+        if not isinstance(directory, str):
+            raise TypeError("The 'directory' argument must be a string type.")
+        if not directory.endswith((".pkl", ".pickle")):
+            raise TypeError("The pickle file directory should end with a '.pkl' or '.pickle' extension.")
+        if not os.path.exists(os.path.split(directory)[0]):
+            if self.verbose:
+                print("Creating {} directory.".format(os.path.split(directory)[0]))
+            os.mkdir(os.path.split(directory)[0])
+        output_dataset = self.find_beige_books()
+        try:
+            with open(directory, "wb") as pickle_output:
+                pickle.dump(output_dataset, pickle_output)
+            return True
+        except(NotImplementedError, FileNotFoundError):
+            return False
 
 
 if __name__ == '__main__':
-  dataset = BeigeBooks().find_beige_books()
-
+    dataset = BeigeBooks().find_beige_books()
+    
