6ec7a9b59c84539797fe631b5d4ce2e96d28464b
David <70294884+David-Woroniuk@users.noreply.github.com>
2022-06-22 22:39:32 +0100

    Update __init__.py

--- FedTools/__init__.py
+++ FedTools/__init__.py
@@ -1,11 +1,14 @@
 from __future__ import print_function
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
+from fake_useragent import UserAgent
+from datetime import datetime
 import re
 import pandas as pd
+import pickle
 import threading
-
-
+import sys
+import os
 
 from .FedMinutes import MonetaryPolicyCommittee
 from .Beigebook import BeigeBooks
